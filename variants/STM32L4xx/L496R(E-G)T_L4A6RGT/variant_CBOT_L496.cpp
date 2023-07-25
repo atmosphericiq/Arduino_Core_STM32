69d52af5416a8a48f0c52d9376ef882d00c3c7fc
fronders <alexandes.zp@gmail.com>
2023-07-25 17:03:32 -0500

    Update variants\STM32L4xx\L496R(E-G)T_L4A6RGT\PeripheralPins_CBOT_L496.c
    enabled RTC clock using LSI source
    removed CRC define as it is on by default

--- variants/STM32L4xx/L496R(E-G)T_L4A6RGT/variant_CBOT_L496.cpp
+++ variants/STM32L4xx/L496R(E-G)T_L4A6RGT/variant_CBOT_L496.cpp
@@ -69,9 +69,11 @@ WEAK void SystemClock_Config(void)
    * Initializes the RCC Oscillators according to the specified parameters
    * in the RCC_OscInitTypeDef structure.
    */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48 | RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48 | RCC_OSCILLATORTYPE_LSI
+                                     | RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
+  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLM = 1;
@@ -95,9 +97,11 @@ WEAK void SystemClock_Config(void)
   }
 
   /* Initializes the peripherals clock */
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB | RCC_PERIPHCLK_SDMMC1;
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB | RCC_PERIPHCLK_SDMMC1
+                                       | RCC_PERIPHCLK_RTC;
   PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_HSI48;
   PeriphClkInit.Sdmmc1ClockSelection = RCC_SDMMC1CLKSOURCE_HSI48;
+  PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_LSI;
   if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
     Error_Handler();
   }
